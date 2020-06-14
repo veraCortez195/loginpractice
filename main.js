9aa7da6a671c003eec4b22be20eb261cda7c4866
Ricardo Guevara Aragón <RickAr2@hotmail.com>
2020-06-13 20:40:51 -0600

    Agregados comentarios con correcciones a los problemas y posibles mejoras

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 class usuario{
     constructor(username, pass){
         this.usuario= username;
@@ -18,47 +16,34 @@ class usuario{
     
 }
 
+// Es preferible utilizar let en lugar de var para declarar variables (y const para constantes)
 var usuarios = [];
 usuarios.push(new usuario("veracortez","vera123"));
 usuarios.push(new usuario("josemendez", "jose123"));
 
-
-
-
-
 function gestionUsuarios(){
-  
-var cajausu= document.getElementById("cajau").value;
-var cajapass= document.getElementById("cajap").value;
-  
-    
- 
-    for(var x=0; x<usuarios.length();x++){
+    var cajausu= document.getElementById("cajau").value;
+    var cajapass= document.getElementById("cajap").value;
 
-        window.alert("dentro de la funcion3" );
+    // length es una variable, no una función, debería ser usuarios.length
+    for(var x=0; x<usuarios.length();x++){
+        alert("dentro de la funcion3" );
+        // getUsuario es una función, pero acá se llama como un objeto o variable, mismo problema con getPass
+        // También, en lugar de utilizar "equal", mejor el operador de igualdad === de la forma dato === caja
         if(usuarios[x].getUsuario.equal(cajausu) && usuarios[x].getPass.equal(cajapass)) 
         {
+            // en el alert sería mejor poner usuarios[x].getUsuario() para saludar al usuario que esta iniciando sesión
             alert("Bienvenida Vera");
         }      
         else if(usuarios[x].getUsuario.equal(cajausu) && usuarios[x].getPass.equal(cajapass)){
-               
+            // Este Else if hace exactamente lo mismo que el if, no es necesario 
             alert("Bienvenido jose");
-
         }
         else{
-            
             window.alert("No se pudo iniciar sesion, compruebe sus credeciales e intente nuevamente");
             
         }
-        
     }
-
-
-    
-    
-
-
-
 }
 
 /*function addnewuser(cajau, cajap){
@@ -71,5 +56,3 @@ var cajapass= document.getElementById("cajap").value;
         Console.log("Nuevo usuario ingresado");
     }
 }*/
-
-
